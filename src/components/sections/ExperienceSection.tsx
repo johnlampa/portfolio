2fe794ecfe73ddb83cb3e5a3bc5f6a00941030ec
John Kyle Lampa <jklampa31@gmail.com>
2025-04-25 20:23:29 +0800

    edit

--- src/components/sections/ExperienceSection.tsx
+++ src/components/sections/ExperienceSection.tsx
@@ -1,34 +1,44 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 function ExperienceSection() {
   return (
-    <div className='flex flex-col items-center text-white'>
-        <div className='text-6xl font-extrabold mb-12 tracking-wider bg-gradient-to-r from-[var(--color-purple)] to-[var(--color-magenta)] bg-clip-text text-transparent'><div>EXPERIENCE</div></div>
-        <div className='flex gap-x-5'>
-            <div className='h-40 w-40 rounded-full overflow-hidden flex items-center justify-center'>
-                <Image src={"public/SysDev.jpg"} alt={""} height={300} width={300}></Image>
-            </div>
-            <div className='flex flex-col justify-center'>
-                <div className='flex justify-between items-end'>
-                    <div className='flex items-end gap-x-1'>
-                        <p className='text-xl font-semibold'>Front End Developer</p>
-                        <p className='text-[var(--color-gray)] opacity-80 italic'>at SAMAHAN Systems Development</p>
-                    </div>
-                    <p className=''>(August 2023 – Present)</p>
-                </div>
-                
-                <ul className='text-[var(--color-gray)] opacity-80 text-sm font-normal mt-5 list-disc list-inside'> 
-                    <li className='mb-1'>Headed the front end team of developers and delivered an informational registration website, using
-                    Next.js and Render</li>
-                    <li className='mb-1'>Ensured that websites were responsive and efficient, which resulted in performant and responsive web
-                    pages</li>
-                    <li className='mb-1'>Collaborated with teams to deliver 4 minor and major projects</li>
-                </ul>
-            </div>
+    <div className="flex flex-col items-center text-white">
+      <div className="text-6xl font-extrabold mb-12 tracking-wider bg-gradient-to-r from-[var(--color-purple)] to-[var(--color-magenta)] bg-clip-text text-transparent">
+        <div>EXPERIENCE</div>
+      </div>
+      <div className="flex gap-x-5">
+        <div className="h-40 w-40 rounded-full overflow-hidden flex items-center justify-center">
+          <Image src={"/SysDev.jpg"} alt={""} height={300} width={300}></Image>
         </div>
+        <div className="flex flex-col justify-center">
+          <div className="flex justify-between items-end">
+            <div className="flex items-end gap-x-1">
+              <p className="text-xl font-semibold">Front End Developer</p>
+              <p className="text-[var(--color-gray)] opacity-80 italic">
+                at SAMAHAN Systems Development
+              </p>
+            </div>
+            <p className="">(August 2023 – Present)</p>
+          </div>
+
+          <ul className="text-[var(--color-gray)] opacity-80 text-sm font-normal mt-5 list-disc list-inside">
+            <li className="mb-1">
+              Headed the front end team of developers and delivered an
+              informational registration website, using Next.js and Render
+            </li>
+            <li className="mb-1">
+              Ensured that websites were responsive and efficient, which
+              resulted in performant and responsive web pages
+            </li>
+            <li className="mb-1">
+              Collaborated with teams to deliver 4 minor and major projects
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ExperienceSection
+export default ExperienceSection;
